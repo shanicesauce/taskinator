85dd5a7cfc2d836085bcb66702dfe310149c5875
shanicesauce <shanicesauceda@gmail.com>
2022-08-05 20:08:44 -0500

    created form to sumit into

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
-var buttonE1 = document.querySelector ("#save-task");
-console.log(buttonE1);
+var formE1 = document.querySelector ("#task-form");
 var taskToDoEl1 = document.querySelector("#task-to-do");
 
-var createTaskHandler= function(){
+var createTaskHandler= function(event){
+    event.preventDefault();
+
     var listItemEl1= document.createElement("li");
     listItemEl1.className = "task-item";
     listItemEl1.textContent = "This is a new task.";
     taskToDoEl1.appendChild(listItemEl1);
 };
 
-buttonE1.addEventListener("click",createTaskHandler);
+formE1.addEventListener("submit", createTaskHandler);
